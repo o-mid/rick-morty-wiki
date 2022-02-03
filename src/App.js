e790675b2931fb8666c3dc84c03987b79dda0086
Omid <omidomirzaei@gmail.com>
2022-02-03 09:50:05 +0330

    first commit - folder Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/filters/Filters";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-1 ">
+        Rick & Morty<span className="text-primary"> Wiki</span>
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div class="col-3  ">
+            <Filters></Filters>
+          </div>
+          <div className="col-8">call8</div>{" "}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/filters/Filters.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Filters = () => {
+  return <div>Filters Filters</div>;
+};
+
+export default Filters;
